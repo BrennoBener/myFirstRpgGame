b64e3cf6cb77af59793a72b9fc8e793921f4b690
BrennoBener <brennobener@outlook.com>
2024-02-29 13:11:02 -0300

    encontrei um bug quando o xp fica em maior quantidade na batalha, corrigi com operador ternario para nao retrnar valor negativo, adicionei funçoes de hit

--- scriptjs/script.js
+++ scriptjs/script.js
@@ -182,7 +182,11 @@ function attack() {
   text.innerText = "The " + monsters[fighting].name + " attacks.";
   text.innerText += " You attack it with your " + weapons[currentWeapon].name + ".";
   health -= getMonsterAttackValue(monsters[fighting].level);
-  monsterHealth -= weapons[currentWeapon].power + Math.floor(Math.random() * xp) + 1;
+  if (isMonsterHit()) {
+    monsterHealth -= weapons[currentWeapon].power + Math.floor(Math.random() * xp) + 1;    
+  } else {
+    text.innerText += " You miss.";
+  }
   healthText.innerText = health;
   monsterHealthText.innerText = monsterHealth;
   if (health <= 0) {
@@ -199,7 +203,11 @@ function attack() {
 function getMonsterAttackValue(level) {
   const hit = (level * 5) - (Math.floor(Math.random() * xp));
   console.log(hit);
-  return hit;
+  return hit > 0 ? hit : 0;
+}
+
+function isMonsterHit() {
+  return Math.random() > .2;
 }
 
 function dodge() {
